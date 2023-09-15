6dfada0e596e7ff603835f999a1de58e839609fd
richie1988 <richiessikaonga@gmail.com>
2023-09-15 22:10:49 +0200

    modified the all js files as requested by the reviewer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import './assets/styletwo.css';
-import homeContent  from './module/homepagedisplayer.js';
+import homeContent from './module/homepagedisplayer.js';
 import { getLike } from './module/likeshandler.js';
 
 window.addEventListener('load', async () => {
