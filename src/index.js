eb73de9a8e2fc48faf739534e5d92b465108368b
RICHARD-SIKAONGA <97953658+richie1988@users.noreply.github.com>
2023-09-15 15:15:14 +0200

    Update index.js
    fix linters error

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import './assets/styletwo.css';
 import homeContent from './module/homepagedisplayer.js';
 import { getLike } from './module/likeshandler.js';
+
 window.addEventListener('load', async () => {
   await homeContent();
   await getLike();
